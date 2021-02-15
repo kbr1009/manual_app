d14ce5051c611eb127856dc3258abe21ac3b05d1
kbr1009 <reo109r@icloud.com>
2021-02-15 22:55:41 +0900

    完成に近くなってきた

--- manual/models.py
+++ manual/models.py
@@ -1,71 +1,84 @@
 from django.db import models
 from adminsortable.models import SortableMixin
+from adminsortable.fields import SortableForeignKey
 
-class Section(models.Model):
-    section_name = models.CharField(verbose_name='セクション', max_length=50, blank=False, null=True)
+class Section(SortableMixin):
+    section_name = models.CharField(verbose_name='1.セクション', max_length=50, blank=False, null=True)
+    the_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+
+    class Meta:
+        verbose_name = '1.セクション'
+        verbose_name_plural = '1.セクション'
+        ordering = ["the_order"]
 
     def __str__(self):
         return self.section_name
 
-    class Meta:
-        verbose_name = 'セクション'
-        verbose_name_plural = 'セクション'
 
-
-class Job(models.Model):
-    job_name = models.CharField(verbose_name='作業名', max_length=50, blank=False, null=True)
+class Job(SortableMixin):
+    job_name = models.CharField(verbose_name='2.作業名', max_length=50, blank=False, null=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE, blank=False, null=True)
+    the_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+
+    class Meta:
+        verbose_name = '2.作業名'
+        verbose_name_plural = '2.作業名' 
+        ordering = ["the_order"]
 
     def __str__(self):
         return self.job_name
 
-    class Meta:
-        verbose_name = '作業名'
-        verbose_name_plural = '作業名'
-
 
 #作業項目
-class Item(models.Model):
-    item_name = models.CharField(verbose_name='作業項目', max_length=50, blank=False, null=True)
+class Item(SortableMixin):
+    item_name = models.CharField(verbose_name='3.作業項目', max_length=50, blank=False, null=True)
     job = models.ForeignKey(Job, on_delete=models.CASCADE, blank=False, null=True)
+    the_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+
+    class Meta:
+        verbose_name = '3.作業項目'
+        verbose_name_plural = '3.作業項目' 
+        ordering = ["the_order"]
 
     def __str__(self):
         return self.item_name
 
-    class Meta:
-        verbose_name = '作業項目'
-        verbose_name_plural = '作業項目' 
-
 
 #作業方法
-class Method(models.Model):
-    method_name = models.CharField(verbose_name='作業方法', max_length=50, blank=False, null=True)
+class Method(SortableMixin):
+    method_name = models.CharField(verbose_name='4.作業方法', max_length=50, blank=False, null=True)
     item = models.ForeignKey(Item, on_delete=models.CASCADE, blank=False, null=True)
+    the_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+
+    class Meta:
+        verbose_name = '4.作業方法'
+        verbose_name_plural = '4.作業方法'
+        ordering = ["the_order"]
+
 
     def __str__(self):
         return self.method_name
 
-    class Meta:
-        verbose_name = '作業方法'
-        verbose_name_plural = '作業方法'
-
 
 #作業手順
-class Procedure(models.Model):
-    procedure_name = models.TextField(verbose_name='作業手順', max_length=50, blank=False, null=True)
+class Procedure(SortableMixin):
+    procedure_name = models.TextField(verbose_name='5.作業手順', max_length=50, blank=False, null=True)
     img = models.ImageField(verbose_name='作業映像', upload_to='img/', blank=True, null=True)
     video = models.FileField(verbose_name='作業動画', upload_to='video/', blank=True, null=True)
     point = models.TextField(verbose_name='ポイント', max_length=50, blank=True, null=True)
     caution = models.TextField(verbose_name='注意点', max_length=50, blank=True, null=True)
     tips = models.TextField(verbose_name='コツ', max_length=50, blank=True, null=True) 
     method = models.ForeignKey(Method, on_delete=models.CASCADE, blank=False, null=True)
+    the_order = models.PositiveIntegerField(default=0, editable=False, db_index=True)
+
+    class Meta:
+        verbose_name = '5.作業手順'
+        verbose_name_plural = '5.作業手順'
+        ordering = ["the_order"]
 
     def __str__(self):
         return self.procedure_name
 
-    class Meta:
-        verbose_name = '作業手順'
-        verbose_name_plural = '作業手順'
 
 """
 class Annotation(models.Model):
