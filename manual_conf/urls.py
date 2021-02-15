607308ef2335553d4227a0ad022b61af8de2fc2e
kbr1009 <reo109r@icloud.com>
2021-02-15 23:17:44 +0900

    adminサイトの表記編集

--- manual_conf/urls.py
+++ manual_conf/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+admin.site.site_title = 'manualapp管理画面' 
+admin.site.site_header = 'ManualApp管理画面' 
+admin.site.index_title = 'メニュー'
+
 urlpatterns = [
     path('manual/', include('manual.urls')),
     path('', include('accounts.urls')),
